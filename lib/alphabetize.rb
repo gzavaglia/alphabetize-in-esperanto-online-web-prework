8efd81fbaf1db4c79a955e618cc3aeb5a3e92bdf
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-11 23:21:44 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ require 'pry'
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort_by do |sentence|
-    sentence.split("").each do |character|
+    sentence.split("").map do |character|
       ESPERANTO_ALPHABET.index(character)
     end
   end
